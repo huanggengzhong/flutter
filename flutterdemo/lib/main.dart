2ff04757a1c0428009c3f5bd942b6966b1ffa5e2
huanggengzhong <915835528@qq.com>
2019-11-04 17:16:58 +0800

    备份

--- flutterdemo/lib/main.dart
+++ flutterdemo/lib/main.dart
@@ -1,26 +1,28 @@
-// 引入flutter包
 import 'package:flutter/material.dart';
 
-// 入口函数
-// void main() => runApp(MyApp());//这是简写
-void main() {
-  return runApp(MyApp());
-}
-// 声明MyApp类
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-// 重写build方法,它返回一个Material风格的组件
   @override
   Widget build(BuildContext context) {
-// 在MaterialApp组件的home属性里赋值一个Scaffold组件
     return MaterialApp(
-      title: "welcome",
-      home: Scaffold(
-// 在Scaffold组件里创建appBar和body
-        appBar: AppBar(title: Text('This is header')),
-// 在body的中间区域添加hello world文本
-        body: Center(child: Text('hello world2')),
-      ),
-      debugShowCheckedModeBanner: false, //去掉右上方的bug
-    );
+        title: 'Text widget',
+        home: Scaffold(
+          body: Center(
+              child: Text(
+                  'hello world,哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+                  textAlign: TextAlign.left,
+                  maxLines:1,
+                  overflow:TextOverflow.ellipsis,
+                  style:TextStyle(
+                    color:Color.fromARGB(255, 255, 0, 150),
+                    fontSize: 25.0,
+                    decoration:TextDecoration.underline,
+                    decorationStyle:TextDecorationStyle.solid
+                    
+                  )
+                  
+                  )),
+        ));
   }
 }
