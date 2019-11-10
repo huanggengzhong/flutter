c2205184dad77493b1fe2bc7ca7ef9acbb3a02fa
huanggengzhong <915835528@qq.com>
2019-11-10 22:32:48 +0800

    上传

--- flutterdemo/lib/main.dart
+++ flutterdemo/lib/main.dart
@@ -1,21 +1,27 @@
+// 动态列表的使用
 import 'package:flutter/material.dart';
-void main()=>runApp(MyApp());
+
+void main()=>runApp(MyApp(
+  // 在这里声明数组,下面是生成1000个数组的方式
+  items:new List<String>.generate(1000,(i)=>"我是第 $i 个item")
+));
+
 class MyApp extends StatelessWidget{
+  // 接收上面的参数
+  final List<String> items;
+  MyApp({Key key,@required this.items}):super(key:key);
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title:'test',
+      title:'materialapp',
       home:Scaffold(
-        appBar: new AppBar(
-          title:new Text("这是头部"),
-          
-        ),
-        body:new ListView(
-          children:<Widget>[
-            new Image.network('https://jspang.com/images/next_blog.jpg'),
-            new Image.network('https://jspang.com/images/next_blog.jpg'),
-            new Image.network('https://jspang.com/images/next_blog.jpg')
-          ]
+        body:new ListView.builder(
+          itemCount:items.length,
+          itemBuilder:(context,index){
+            return new ListTile(
+              title:new Text('${items[index]}')
+            );
+          }
         )
       )
     );
